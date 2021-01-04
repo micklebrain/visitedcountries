db0bbb22f523f84796016ab5843b25309961315a
Nathan Nguyen <nathannguyen@nathans-mbp-2.home>
2021-01-03 21:20:59 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Lost Minds (Travel Blog)
+        </h1>
+        <h1> New York </h1>
+        <p> Indoor dining closed </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/IndoorDining.js
@@ -0,0 +1,14 @@
+class IndoorDining extends React.Component {
+    render() {
+        return <div> 
+        <h1> US cities </h1>
+        <h1> New York City</h1>   
+        <p> Indoor Dinining set to close Monday December 14 </p>
+        <h1> Los Angeles </h1>
+        <p> Los Angeles </p>
+        <h1> Chicago </h1>
+        <h2> Philedelphia </h2>
+        <p> Indoor dining is closed indefinitely </p> 
+        </div>;
+    }
+}
