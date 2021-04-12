102ed5af86c6db3e9b5cf2a4572861438cf22d62
Nathan Nguyen <nathannguyen@nathans-mbp-2.home>
2021-04-12 15:37:11 -0400

    KOFI

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <body>
         <h1>
           Lost Minds (Travel Blog)
         </h1>
-        <h2>Confirmed locations with indoor dining open </h2>
-          <ul>
-            <li> New Jersey last updated - 1/3/21 </li>                    
-          </ul> 
         <h2>Confirmed locations with indoor dining closed</h2>
         <ul>
-          <li> New York last updated - 1/3/21 </li>        
+          <li> New York last updated - 1/3/21 </li>
           <li> Puerto Rico last updated - 1/3/21 </li>
-        </ul> 
-      </header>
+        </ul>
+        <h2>Confirmed locations with indoor dining open </h2>
+        <ul>
+          <li> New Jersey last updated - 1/3/21 </li>
+          <li> Georgia last updated - 1/3/21 </li>
+        </ul>
+      </body>
     </div>
   );
 }
