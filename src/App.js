9762a45af2585947e3bdf4a15fbe58679abbac14
Nathan Nguyen <nathannguyen@nathans-mbp-2.home>
2021-04-12 15:42:19 -0400

    Update indoor list

--- src/App.js
+++ src/App.js
@@ -12,14 +12,17 @@ function App() {
           Lost Minds (Travel Blog)
         </h1>
         <h2>Confirmed locations with indoor dining closed</h2>
-        <ul>
-          <li> New York last updated - 1/3/21 </li>
+        <ul>          
           <li> Puerto Rico last updated - 1/3/21 </li>
         </ul>
-        <h2>Confirmed locations with indoor dining open </h2>
+        <h2>Confirmed locations with partial indoor dining open </h2>
         <ul>
-          <li> New Jersey last updated - 1/3/21 </li>
-          <li> Georgia last updated - 1/3/21 </li>
+          <li> New York last updated - 4/12/21 </li>
+        </ul>
+        <h3>Confirmed locations with complete indoor dining open </h3>
+        <ul>          
+          <li> New Jersey last updated - 4/12/21 </li>
+          <li> Georgia last updated - 4/12/21 </li>
         </ul>
       </body>
     </div>
