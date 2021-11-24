b3cdae491293cb1908fbf98cef17f35358abdce9
Nathan Nguyen <micklebrain@gmail.com>
2021-11-23 23:16:05 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -9,23 +9,8 @@ import {
   Route,
   Link,
 } from "react-router-dom";
-import Denver from './cities/Denver';
-import NewYorkCity from './cities/NewYorkCity';
-import TopVegasResturants from './TopVegasResturants';
-import TicketMarketplace from './TicketMarketplace';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import NewYorkCityArticle from './NYCArticles/NewYorkCityArticle';
-import NewYorkCityArticle2 from './NYCArticles/NewYorkCityArticle2';
-import NewYorkCityArticle3 from './NYCArticles/NewYorkCityArticle3';
-import NewYorkCityArticle4 from './NYCArticles/NewYorkCityArticle4';
-import Article from './Article'
 
 class App extends React.Component {
-  
-  // ListItemLink(props) {
-  //   return <ListItem button component="a" {...props} />;
-  // } 
 
   render() {   
     return (
