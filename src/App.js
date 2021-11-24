fb51cad0dc80cf8621fcf95372af64f54d4d6e19
Nathan Nguyen <micklebrain@gmail.com>
2021-11-23 23:12:30 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -35,57 +35,13 @@ class App extends React.Component {
             <ul>
               <li>
                 <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/ticketMarketplace">Ticket Marketplace</Link>
-              </li>                      
-              {/* 
-              bars
-              nightclubs 
-              general attractions 
-              tour guide
-              festivals
-              hotels
-              speakeasies
-              */}
+              </li>                  
             </ul>
           </nav>
-          <div style = { {
-            width: '100%',
-            maxWidth: 360,
-            } }>
-            <List component="nav" aria-label="secondary mailbox folders" >
-              Cities
-              <ListItem button>
-                <Link to="/denver">Denver</Link>      
-              </ListItem>
-              <ListItem button>
-                <Link to="/lasvegas">Las Vegas</Link>       
-              </ListItem>
-              <ListItem button>
-                <Link to="/newyorkcity">New York City</Link>       
-              </ListItem>         
-              {/* <ListItemLink href="#simple-list">
-                <ListItemText primary="Spam" />
-              </ListItemLink> */}
-            </List>
-          </div>
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/speakeasies" component={Speakeasy} />
-            <Route exact path="/denver" component={Denver} />
-            <Route exact path="/newyorkcity" component={NewYorkCity} />                        
-            <Route exact path="/topvegasresturants" component={TopVegasResturants} />
-            <Route exact path="/hotels" component={Hotels} />   
-            <Route exact path="/ticketMarketplace" component={TicketMarketplace} />
-            
-            <Route exact path="/article" component={Article} />
-            <Route exact path="/NYCarticle" component={NewYorkCityArticle} />
-            <Route exact path="/NYCarticle2" component={NewYorkCityArticle2} />
-            <Route exact path="/NYCarticle3" component={NewYorkCityArticle3} />
-            <Route exact path="/NYCarticle4" component={NewYorkCityArticle4} />
           </Switch>          
       </Router>      
     </div>)
--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,7 @@ function Home() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <h1> Lost Minds </h1>
-        <h2> Itinerary to travel USA cities so you don't waste time </h2>
+        <h1> Asian baby girls </h1>        
       </div>
     );
   }
