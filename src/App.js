3a00a5b5d3b4d24f3b0215c46ca3c50d5234bc81
Nathan Nguyen <micklebrain@gmail.com>
2021-12-21 14:12:35 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import React from 'react';
-import Home from './Home';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-
-import Navbarmenu from './Navbarmenu';
-
 
 class App extends React.Component {
   render() {   
