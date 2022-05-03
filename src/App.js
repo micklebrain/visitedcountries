51a664b6780ab0f5765771597bc06ab5d7403cb5
Nathan Nguyen <micklebrain@gmail.com>
2022-05-03 15:30:44 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
   render() {   
     return (
     <div>
+      <img src="http://chart.apis.google.com/chart?cht=map:fixed=-70,-180,80,180&chs=450x300&chf=bg,s,336699&chco=d0d0d0,cc0000&chd=s:99999999&chld=CO|US|CA|GB|FR|VN|CN|JP" width="450" height="300" ></img><br/>visited 8 states (3.55%)<br/>
+      <img src="http://chart.apis.google.com/chart?cht=map:fixed=40,-150,75,-50&chs=455x330&chf=bg,s,336699&chco=d0d0d0,cc0000&chd=s:9&chld=CA-ON" width="455" height="330" ></img><br/>visited 1 states (7.69%)<br/>
       <Router>
           <Switch>
             <Route exact path="/">              
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
       
-      <div className="App">         
+      <div className="App">              
       </div>
     );
   }
